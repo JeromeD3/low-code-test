ead446587af5aa10091b478d3ee5b710520f0e8d
邓祥柏 <1760399176@qq.com>
2024-03-07 23:08:58 +0800

    feat: 自定义环境变量

--- apps/low-code-test/src/app.controller.ts
+++ apps/low-code-test/src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller({
   version: '1',
-  path: 'user',
 })
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('getTestName')
+  getTestName() {
+    return this.configService.get('TEST_VALUE').name;
+  }
 
   @Get()
   findAll() {
