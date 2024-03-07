0c558e2e71a5a1c0d9dca5c69d301ea7967c8001
邓祥柏 <1760399176@qq.com>
2024-03-07 23:21:10 +0800

    feat: 新增Swagger

--- apps/low-code-test/src/main.ts
+++ apps/low-code-test/src/main.ts
@@ -4,6 +4,7 @@ import { VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,9 @@ async function bootstrap() {
     defaultVersion: '1',
     type: VersioningType.URI,
   });
+
+  // 创建文档
+  generateDocument(app);
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   await app.listen(3000);
